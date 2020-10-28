6dd755383fe0f1d3c894e625de63975ea32ddcbb
unknown <baruchmargolis@gmail.com>
2020-10-28 18:00:25 +0200

    Added open File in Father

--- HW1/Father/main.c
+++ HW1/Father/main.c
@@ -10,11 +10,19 @@ Processes
 /* Library includes: */
 #include <stdio.h> /* for printf */
 #include <stdlib.h> /* for exit() */
-#include <windows.h>
 
 int main(int argc, char** argv) {
 
+	
 
+	FILE* fp_in = NULL, * fp_out = NULL;
+	if (NULL == (fp_in = fopen(argv[1], "r")))
+	{
+		printf("Error: opening %s failed\n", argv[1]);
+		exit(1);
+	}
+	char line[100];
+	fgets(line, 100, fp_in);
 	return 0;
 
 }
